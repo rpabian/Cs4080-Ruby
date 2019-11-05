1541a43d690a104ac6a411498c3fd7e42e112b78
Mora <mlabisi98@gmail.com>
2019-11-05 10:39:39 -0800

    Update demo.rb

--- demo.rb
+++ demo.rb
@@ -5,28 +5,34 @@ Comments
 =end
 
 def say_hello(name)
-   #with out new line
    print "Hello, "
-  # with new line
    puts  "#{name}"
    puts "Welcome to Ruby!"
 
 end
 
-#without return
 def add2(num)
     num+2
 end
 
-#with return
+def isConsecutive(array)
+   lastNum = array[0]
+   array[1, array.count].each do |num|
+      if lastNum != num - 1
+         return false
+      end
+      lastNum = num
+   end
+   true
+end
+
 def getSum(array)
     result = array.inject(0){|sum,x| sum + x }
     return result
 end
 
-say_hello('Everyone')
+say_hello('Dr. Yang')
 puts add2(2)
 array = [1,2,3]
+puts isConsecutive(array)
 puts getSum(array)
-
-
