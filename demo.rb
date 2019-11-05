f5d75fce61327d50468face396df978129de1221
gwtw2525 <40869374+gwtw2525@users.noreply.github.com>
2019-11-05 10:29:22 -0800

    Update

--- demo.rb
+++ demo.rb
@@ -5,22 +5,26 @@ Comments
 =end
 
 def say_hello(name)
+   #with out new line
    print "Hello, "
+  # with new line
    puts  "#{name}"
    puts "Welcome to Ruby!"
 
 end
 
+#without return
 def add2(num)
     num+2
 end
 
+#with return
 def getSum(array)
     result = array.inject(0){|sum,x| sum + x }
     return result
 end
 
-say_hello('Diane')
+say_hello('Everyone’')
 puts add2(2)
 array = [1,2,3]
 puts getSum(array)
