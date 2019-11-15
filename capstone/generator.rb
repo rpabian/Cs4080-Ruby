0efc5a530a5377079dc7cde561a1fd6cdcf6fd5e
Mora <mlabisi98@gmail.com>
2019-11-14 17:53:44 -0800

    Third draft of capstone initialization

--- capstone/generator.rb
+++ capstone/generator.rb
@@ -1,52 +1,25 @@
 # frozen_string_literal: true
 
-mydir = __dir__
+require 'i18n'
+
+I18n.load_path += Dir[File.join( __dir__, 'dummy-data', '*.yml')]
 
 module Generator
 
-  class Main
-    # DEFINE GLOBAL ALPHABET FOR GENERATOR (LETTERS, NUMBERS)
-
+  class Engine
+    
     class << self
 
-      def numerify(number_string)
-        # GENERATE A STRING OF DIGITS FROM A GIVEN STRING
+      def parse(key)
+        # FETCH
       end
 
-      def letterify(letter_string)
-        # GENERATE A STRING OF LETTERS FROM THE GIVEN STRING
-      end
-
-      def alphanumerify(string)
-        # GENERATE AN ALPHANUMERIC STRING FROM THE GIVEN STRING
-      end
-
-      def generate(key)
-        # GENERATE THE RANDOM VALUE
-      end
-
-      def rand(max = nil)
-        # RANDOM FUNCTION
-      end
-
-      def rand_in_range(from, to)
-        # SAMPLE (GRAB A RANDOM ELEMENT) FROM THE GIVEN RANGE
-      end
-
-      def randomize(value)
-        # RANDOMIZE GIVEN VALUE IF POSSIBLE
-      end
-
-      def sample(list)
-        # SAMPLE FUNCTION (GRAB RANDOM ELEMENT FROM ARRAY)
-      end
-
-      def shuffle(list)
-        # SHUFFLE FUNCTION
+      def fetch(key)
+        # FETCH
       end
     end
   end
 end
 
 # This line registers all of the generators
-Dir.glob(File.join(File.dirname(__FILE__), 'generators', '/**/*.rb')).sort.each { |file| require file }
+Dir.glob(File.join(File.dirname(__FILE__), 'generators', '*.rb')).sort.each { |file| require file }
---
+++ capstone/generators/cpp_building.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module Generator
+  class Building < Engine
+
+    class << self
+      def building_number
+        
+      end
+
+      def building_name
+
+      end
+    end
+  end
+end
