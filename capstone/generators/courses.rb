d89e1bb081ee9c87676e3d2e99bca5e4d8cb6654
Mora <mlabisi98@gmail.com>
2019-12-04 13:44:23 -0800

    Create courses.rb

---
+++ capstone/generators/courses.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Generator
+  class Course < Base
+    flexible :course
+
+    class << self
+      def course_subject
+        fetch('course.subject')
+      end
+
+      def course_number
+        Array.new(4) { |_| fetch(digits) }.join 
+      end
+
+      def digits
+        %w[0 1 2 3 4 5 6 7 8 9]
+      end
+    end
+  end
+end
