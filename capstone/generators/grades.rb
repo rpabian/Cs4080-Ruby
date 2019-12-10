537181f88da6bee29a55800c0e1fe8728325315d
gwtw2525 <40869374+gwtw2525@users.noreply.github.com>
2019-12-09 18:37:22 -0800

    update grades.rb

--- capstone/generators/grades.rb
+++ capstone/generators/grades.rb
@@ -4,13 +4,6 @@ module DBSeeder
       def grade_letter
         fetch('grade.letter')
       end
-
-      def course_number
-        Array.new(3) { |_| sample(digits) }).join
-      end
-      def digits
-        %w[0 1 2 3 4 5 6 7 8 9]
-      end
     end
   end
 end
